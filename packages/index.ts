86ab8d9b41708598a43b27906f80a9d44c293d4e
yutoiwama <yutoiwama@gmail.com>
2024-02-08 18:20:46 +0900

    createApp
    next:
    https://ubugeeei.github.io/chibivue/10-minimum-example/010-create-app-api.html#%E3%83%AA%E3%83%95%E3%82%A1%E3%82%AF%E3%82%BF%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%99

--- packages/index.ts
+++ packages/index.ts
@@ -1,3 +1,18 @@
-export const helloChibivue = () => {
-  console.log('Hello chibivue!');
+export type Options = {
+  render: () => string;
+};
+
+export type App = {
+  mount: (selector: string) => void;
+};
+
+export const createApp = (options: Options): App => {
+  return {
+    mount: (selectors: string) => {
+      const root = document.querySelector(selectors);
+      if (root) {
+        root.innerHTML = options.render();
+      }
+    },
+  };
 };
